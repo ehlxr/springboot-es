650156569d0893248f8f7f579a2b2fbdaf29d98c
ehlxr <ehlxr.me@gmail.com>
2021-03-07 21:08:22 +0800

    update at 2021-03-07 21:08:22 by ehlxr

--- src/test/java/io/github/ehlxr/es/SpringbootEsApplicationTests.java
+++ src/test/java/io/github/ehlxr/es/SpringbootEsApplicationTests.java
@@ -1,9 +1,23 @@
 package io.github.ehlxr.es;
 
+import io.github.ehlxr.es.utils.JsonUtils;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.Aggregations;
@@ -16,6 +30,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
 @SpringBootTest
 class SpringbootEsApplicationTests {
@@ -23,8 +38,62 @@ class SpringbootEsApplicationTests {
     @Autowired
     private RestHighLevelClient restHighLevelClient;
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @ToString
+    static class User {
+        private String name;
+        private Integer gender;
+        private Integer age;
+        private String email;
+    }
+
     @Test
-    void searchData() throws IOException {
+    void indexTest() throws IOException {
+        IndexRequest request = new IndexRequest("my_idx");
+        request.id("10");
+        request.ifSeqNo();
+        request.source(JsonUtils.obj2String(new User("zhansan", 1, 23, "zhansan@test.com")), XContentType.JSON);
+
+        IndexResponse response = restHighLevelClient.index(request, RequestOptions.DEFAULT);
+        System.out.println(response);
+    }
+
+    @Test
+    void deleteIndexTest() throws IOException {
+        GetIndexRequest getIndexRequest = new GetIndexRequest("my_idx");
+        boolean exists = restHighLevelClient.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
+        if (exists) {
+            DeleteIndexRequest request = new DeleteIndexRequest("my_idx");
+            request.timeout(TimeValue.timeValueMinutes(2));
+
+            AcknowledgedResponse response = restHighLevelClient.indices().delete(request, RequestOptions.DEFAULT);
+            System.out.println(response.isAcknowledged());
+        }
+    }
+
+    @Test
+    void bulkTest() throws IOException {
+        BulkRequest request = new BulkRequest("my_idx");
+
+        IntStream.range(0, 10).forEach(i -> {
+            IndexRequest indexRequest = new IndexRequest("my_idx")
+                    .id("" + i)
+                    .source(JsonUtils.obj2String(new User("zhansan" + 1, i % 2, 20 + i, i + "zhansan@test.com")), XContentType.JSON);
+
+            System.out.println(indexRequest.toString());
+            request.add(indexRequest);
+        });
+
+
+        BulkResponse response = restHighLevelClient.bulk(request, RequestOptions.DEFAULT);
+        System.out.println(response.hasFailures());
+    }
+
+
+    @Test
+    void searchTest() throws IOException {
         /*
          * GET bank/_search
          * {
